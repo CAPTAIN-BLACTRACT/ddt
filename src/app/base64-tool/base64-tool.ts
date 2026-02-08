98a64366cd9a9ff2aa92b7e406d41a003e1ddf34
Ayush <iiser.ayush@gmail.com>
2026-02-08 10:46:41 +0530

    ft: added plaintText to Base64 Conversion Feature

--- src/app/base64-tool/base64-tool.ts
+++ src/app/base64-tool/base64-tool.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component,signal } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 @Component({
   selector: 'app-base64-tool',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './base64-tool.html',
   styleUrl: './base64-tool.css',
 })
 export class Base64Tool {
+
+  plainText= signal('');
+  base64Text =  signal('');
+
+  encode(val:string){
+    this.plainText.set(val);
+    try {
+      this.base64Text.set(btoa(val));
+    } catch (e) {
+
+    }
+  }
+  decode(val:string){
+    this.base64Text.set(val);
+    try {
+      this.plainText.set(atob(val));
+    } catch (e) {
+
+    }
+  }
 
 }
